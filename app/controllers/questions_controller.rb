b14933009c25cdcc0da0fabeb456fa6ce9ec4fb5
Vincent Donneaud vincent.donneaud@gmail.com <vincent.donneaud@gmail.com>
2018-11-19 14:17:31 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    #raise
+  end
+end
